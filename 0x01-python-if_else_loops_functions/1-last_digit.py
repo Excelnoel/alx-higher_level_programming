510bc91fe976205295ed3e4eb450300e7ab9d9ac
Excelnoel <mannymacadex@gmail.com>
2023-11-28 13:03:35 -0800

    Auto-commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,8 +13,8 @@ print("Last digit of", number, "is", digit, end=" ")
 if digit > 5:
     print("and is greater than 5")
 elif digit < 0:
-    print("and is less than 0")
-elif digit == 0:
     print("and is less than 6 and not 0")
-#else:
-#    print("and is less than 6 and not 0")
+elif digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
