d0fbe475fa84b7e051218493e51b34ef045a275d
Excelnoel <mannymacadex@gmail.com>
2023-11-28 14:31:59 -0800

    Auto-commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-# 11-pow.py
-
-
-def pow(a, b):
-    """Return a to the power of b."""
-    return (a ** b)
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
