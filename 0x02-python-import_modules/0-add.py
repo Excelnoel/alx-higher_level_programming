66488c62bb77f8592081b01314f35f4611f30fb1
Excelnoel <mannymacadex@gmail.com>
2023-11-30 08:30:42 -0800

    Auto-commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ =="__main__":
-    a = 1
-    b = 2
-    print ("(:d) + (:d) = (:d)" .format(a, b, add(a, b)))
+a = 1
+b = 2
+add_0 = __import__("add_0").add
+print("{} + {} = {}" .format(a, b, add_0(a, b)))
