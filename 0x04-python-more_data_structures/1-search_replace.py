5b2870d4f81dea400b044af9bc2372e3617499ff
Excelnoel <mannymacadex@gmail.com>
2023-12-06 06:25:26 -0800

    Auto-commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    new_list = [replace if item == search else item for item in my_list]
+    new_list = []
+    for num in my_list:
+        if num == search:
+            new_list.append(replace)
+        else:
+            new_list.append(num)
     return new_list
